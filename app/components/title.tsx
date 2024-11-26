8850383627266dcca2064e0a99d6126cda21934f
Lautaro Roldan <lautaronicolasroldan@gmail.com>
2024-11-26 16:53:06 -0300

    estilos para el title

--- app/components/title.tsx
+++ app/components/title.tsx
@@ -1,4 +1,5 @@
 import { cn } from '@/lib/utils';
+import { Poppins } from 'next/font/google';
 import React from 'react'
 
 interface TitleProps {
@@ -6,9 +7,14 @@ interface TitleProps {
     className?: string;
 }
 
+const poppins = Poppins({
+    subsets: ['latin'],
+    weight: ['400', '500', '600', '700'],
+})
+
 function Title({ title, className }: TitleProps) {
     return (
-        <h1 className={cn('text-4xl font-bold text-white', className)}>{title}</h1>
+        <h1 className={cn(`text-4xl font-bold text-white ${poppins.className} capitalize`, className)}>{title}</h1>
     )
 }
 
